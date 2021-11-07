70127abcb68206d47818ead0fc70a42a149f05db
Gataullin Ruslan <rusikarm3@gmail.com>
2021-11-07 11:56:11 +0300

    rename test files

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    graph graph = graph_reader::from_file("graph2.txt");
+    graph graph = graph_reader::from_file("test2.txt");
     std::cout << graph.max_flow(1, 6) << std::endl;
     return 0;
 }
